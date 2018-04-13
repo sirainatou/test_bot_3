f8a74f035f75efc0dbe86316ae0bfe8b28509041
sirine bouslama <sirine_bouslama@hotmail.fr>
2018-04-13 16:34:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    #response_sent_text = get_message()
-                    #send_message(recipient_id, response_sent_text)
+                    response_sent_text = get_message()
+                    send_message(recipient_id, response_sent_text)
                     #send_buttons_case1(recipient_id)
                     #jbot.send_raw(payload) 
                     print('ok')
